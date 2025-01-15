888402ff0e4aee66e34ab336f704320826c142b5
Mohamed-Amine-KHLIF <mohamed-amine.khlif@telecom-sudparis.eu>
2025-01-15 10:28:44 +0100

    frontend

--- program/main.py
+++ program/main.py
@@ -1,6 +1,5 @@
 import inquirer
 import sys
-
 from os_class import OsManip, PkgInstaller, PkgConfig
 
 def menu_installer(distribution):
@@ -23,42 +22,75 @@ def menu_installer(distribution):
     answers = inquirer.prompt([
         inquirer.Checkbox(
             'installer_selection',
-            message="Which security packages should be present on the system?",
+            message="Which security packages should be present on the system ?",
             choices=options,
             default=installed
         )
     ])
+    
     for pk in answers['installer_selection']:
         if pk == "auditd":
             installer.install_package("wget")
             installer.install_package("auditd")
-            # TODO: ask user for default rules + enable program
-            # config.auditd(running_status, boot_status, install_rules = False)
+            # Ask user for default rules and enable program
+            auditd_answers = inquirer.prompt([
+                inquirer.Confirm('enable_auditd', message="Enable auditd program?", default=True),
+                inquirer.Confirm('install_default_rules', message="Install default auditd rules?", default=True)
+            ])
+            config.auditd(auditd_answers['enable_auditd'], True, install_rules=auditd_answers['install_default_rules'])
 
         elif pk == "apparmor":
             installer.install_package("apparmor apparmor-utils apparmor-profiles")
-            # TODO: Ask user for options
-            # roddhjav's profiles (https://github.com/roddhjav/apparmor.d)
-            # config.apparmor(self, running_status, boot_status, install_rules = False, force_enforcing = False, run_on_boot = False)
+            # Ask user for options
+            apparmor_answers = inquirer.prompt([
+                inquirer.Confirm('enable_apparmor', message="Enable AppArmor?", default=True),
+                inquirer.Confirm('install_rod_profiles', message="Install roddhjav's profiles?", default=True),
+                inquirer.Confirm('enforce_apparmor', message="Enforce AppArmor policies?", default=True),
+                inquirer.Confirm('run_on_boot', message="Enable AppArmor to run on boot?", default=True)
+            ])
+            config.apparmor(
+                apparmor_answers['enable_apparmor'], 
+                True, 
+                install_rules=apparmor_answers['install_rod_profiles'],
+                force_enforcing=apparmor_answers['enforce_apparmor'], 
+                run_on_boot=apparmor_answers['run_on_boot']
+            )
 
         elif pk == "fail2ban":
             installer.install_package("fail2ban")
-            # TODO config
+            # TODO config: Add questions and call config.fail2ban
 
         elif pk == "firewalld":
             installer.remove_package("iptables-persistent")
             installer.remove_package("ufw")
             installer.install_package("firewalld")
-            # TODO: ask user for default rules + enable program
-            # config.firewalld(self, running_status, boot_status, set_defaults = False, ipv6 = False, docker = False):
-        
+            # Ask user for default rules and enable program
+            firewalld_answers = inquirer.prompt([
+                inquirer.Confirm('enable_firewalld', message="Enable firewalld?", default=True),
+                inquirer.Confirm('set_default_rules', message="Set default firewall rules?", default=True),
+                inquirer.Confirm('ipv6_support', message="Enable IPv6 support?", default=False),
+                inquirer.Confirm('docker_support', message="Enable Docker support?", default=False)
+            ])
+            config.firewalld(
+                firewalld_answers['enable_firewalld'], 
+                True, 
+                set_defaults=firewalld_answers['set_default_rules'],
+                ipv6=firewalld_answers['ipv6_support'], 
+                docker=firewalld_answers['docker_support']
+            )
+
         elif pk == "unattended-upgrades":
             installer.install_package("unattended-upgrades")
-            # TODO config
+            # TODO config: Add questions and call config.unattended_upgrades
 
-    # TODO: pkg uninstall logic when installed on the system and unselected by user
-    # Ask for user confirmation per package!!!!!
-
+    # uninstall logic when package was installed on the system and unselected by user
+    for pk in options:
+        if pk not in answers['installer_selection'] and installer.get_package(pk):
+            uninstall_answer = inquirer.prompt([
+                inquirer.Confirm(f"uninstall_{pk}", message=f"Do you want to uninstall {pk}?", default=False)
+            ])
+            if uninstall_answer[f"uninstall_{pk}"]:
+                installer.remove_package(pk)
 
 if __name__ == "__main__":
     os_manip = OsManip()
